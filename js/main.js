575ccb1065c85ed088e41a2a6d764545ccfc99fb
salmanbahaev <bahaev.sm@gmail.com>
2021-08-03 19:17:05 +0300

    added slider

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
-  $('.header__burger').click(function(event) {
+  $('.header__burger, .header__site-menu').click(function(event) {
     $('.header__burger,.header__menu').toggleClass('active')
     $('body').toggleClass('lock')
+  })
+
+  $('.banner__slider').slick({
+    dots: true,
+    speed: 1000,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    pauseOnDotsHover: true,
+    // lazyLoad: 'ondemand',
+    prevArrow: '<button type="button" class="slick__banner-prev"><img class="banner__arrow-svg" src="img/arrow-banner.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick__banner-next"><img class="banner__arrow-svg" src="img/arrow-banner.svg" alt=""></button>'
+  });
+
+
+  $('.specialists__slider').slick({
+    dots: true,
+    speed: 500,
+    infinite: false,
+
+    prevArrow: '<button type="button" class="slick__banner-prev"><img class="banner__arrow-svg" src="img/arrow-banner.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick__banner-next"><img class="banner__arrow-svg" src="img/arrow-banner.svg" alt=""></button>'
   })
 })
