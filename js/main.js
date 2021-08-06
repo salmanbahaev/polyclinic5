be66be8906f2fa8cdc3d03d9e91fc99ad89845d5
salmanbahaev <bahaev.sm@gmail.com>
2021-08-06 16:08:34 +0300

    dada

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-  $('.header__burger, .header__site-menu').click(function(event) {
+  $('.header__burger, .header__site-menu').click(function() {
     $('.header__burger,.header__menu').toggleClass('active')
     $('body').toggleClass('lock')
+    $('.header').toggleClass('header__center')
   })
 
+  // слайдер для баннера
   $('.banner__slider').slick({
     dots: true,
     speed: 1000,
@@ -17,12 +19,11 @@ $(document).ready(function() {
         arrows : false
       }
     }],
-
     prevArrow: '<button type="button" class="slick__banner-prev"><img class="banner__arrow-svg" src="img/arrow-banner.svg" alt=""></button>',
     nextArrow: '<button type="button" class="slick__banner-next"><img class="banner__arrow-svg" src="img/arrow-banner.svg" alt=""></button>'
   });
 
-
+  // слайдер для специалистов
   $('.specialists__slider').slick({
     speed: 500,
     infinite: true,
@@ -34,11 +35,11 @@ $(document).ready(function() {
         arrows : false,
       }
     }],
-
     prevArrow: '<button type="button" class="slick__banner-prev"><img class="banner__arrow-svg" src="img/arrow-banner.svg" alt=""></button>',
     nextArrow: '<button type="button" class="slick__banner-next"><img class="banner__arrow-svg" src="img/arrow-banner.svg" alt=""></button>'
   })
 
+  // слайдер для карточек-ссылок
   $('.healthlinks__slider').slick({
     speed: 500,
     infinite: true,
@@ -50,10 +51,22 @@ $(document).ready(function() {
         arrows : false
       }
     }],
-
     prevArrow: '<button type="button" class="slick__banner-prev"><img class="banner__arrow-svg" src="img/arrow-banner.svg" alt=""></button>',
     nextArrow: '<button type="button" class="slick__banner-next"><img class="banner__arrow-svg" src="img/arrow-banner.svg" alt=""></button>'
   })
 
+  function shineLinks(id){
+    try{
+      var el = document.getElementById(id).getElementsByTagName('a');
+      var url = document.location.href;
+      for (var i = 0; i < el.length; i++){
+        if (url == el[i].href) {
+            el[i].className += ' act';
+        };
+      };
+    } catch(e){}
+  };
+
+  shineLinks('menu-act');
   
 })
